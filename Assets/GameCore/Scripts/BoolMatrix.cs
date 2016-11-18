192d7e4751ddeac3af1bab9f148cd0c07c73ddd1
Bartosz Nowotny <mapman17@gmail.com>
2016-11-18 18:22:57 +0100

    Extract BoolMatrix to separate class file

---
+++ Assets/GameCore/Scripts/BoolMatrix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class BoolMatrix
+{
+    [SerializeField]
+    RowData[] row;
+
+    public RowData[] Row
+    {
+        get
+        {
+            return row;
+        }
+        set
+        {
+            row = value;
+        }
+    }
+
+    public BoolMatrix( int size )
+    {
+        Row = new RowData[size];
+        for ( int i = 0; i < size; i++ )
+        {
+            Row[i] = new RowData( size );
+        }
+    }
+
+    [Serializable]
+    public class RowData
+    {
+        [SerializeField]
+        bool[] column;
+
+        public RowData( int size )
+        {
+            Column = new bool[size];
+        }
+
+        public bool[] Column  // Column, so it can be accessed like Rows[0].Column[1];
+        {
+            get
+            {
+                return column;
+            }
+            set
+            {
+                column = value;
+            }
+        }
+    }
+}
